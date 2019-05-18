335210fb21adbe4d3ec56890d504ae9246dbb5bf
Harshvardhan Ojha <42267022+hv-ojha@users.noreply.github.com>
2019-05-18 22:50:58 +0530

    Create Compare-the-Triplets.java

---
+++ Compare-the-Triplets.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int ag=0,bg=0,i;
+        List<Integer> l1 = new ArrayList<Integer>();
+        l1.add(0,ag);
+        l1.add(1,bg);
+        for(i=0;i<3;i++)
+        {
+            if(a.get(i)>b.get(i))
+            {
+                ag++;
+                l1.set(0,ag);
+            }
+            if(a.get(i)<b.get(i))
+            {
+                bg++;
+                l1.set(1,bg);
+            }
+        }
+        return l1;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
