2e650112d24d2a98993758390fe446fcfe137f00
Harshvardhan Ojha <42267022+hv-ojha@users.noreply.github.com>
2019-05-18 23:27:31 +0530

    Create Day-1-Data-Types.java

---
+++ Day-1-Data-Types.java
@@ -0,0 +1,19 @@
+
+
+        /* Declare second integer, double, and String variables. */
+        Scanner in = new Scanner(System.in);
+        int j=in.nextInt();
+        double e=in.nextDouble();
+        String t=in.next();
+        t+=in.nextLine();
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+j);
+        System.out.println(d+e);
+        System.out.println(s+t);
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
